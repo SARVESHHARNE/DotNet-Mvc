885a73fa0933a644ce92e9ccae3b35943cb04bfc
SARVESHHARNE <harnesarvesh@gmail.com>
2024-01-20 19:51:20 +0530

    20-01-2024

---
+++ Using EntityFramework/WebApplication2/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options) { 
+            
+        }
+    }
+}
---
+++ Using EntityFramework/WebApplication2/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Email {  get; set; }
+
+        public int Phone { get; set; }
+    }
+}
